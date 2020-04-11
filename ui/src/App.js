c1c476fccdede60109ad8fa6be05c04a65c4d190
beni-gut <beni-gut@users.noreply.github.com>
2020-04-11 14:20:49 +0200

    start der react-komponenten, react router als nächstes

--- ui/src/App.js
+++ ui/src/App.js
@@ -18,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+        <h1>Test, does it reload when i just type something</h1>
+        <p>Incredible</p>
       </header>
     </div>
   );
