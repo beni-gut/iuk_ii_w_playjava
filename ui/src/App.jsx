86771b99c2251a76f4cc1157b8974bdb6d6cdf90
beni-gut <beni-gut@users.noreply.github.com>
2020-04-21 12:27:11 +0200

    React Router more or less implemented

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,118 +1,48 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-      Switch,
-      Route,
-      Link,
-      useRouteMatch,
-      useParams
-} from "react-router-dom";
-import logo from './logo.svg';
+
+import logo from './book.svg';
+import Books from "./book/Books.jsx"
+import CreateBook from "./book/CreateBook";
 import './App.css';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+
 
 function App() {
   return (
-      <Router>
-          <div>
-              <nav className={"navbar"}>
-                  <ul>
-                      <li className={"App-Link"}>
-                          <Link to={"/"}>Home</Link>
-                      </li>
-                      <li>
-                          <Link to={"/about"}>About</Link>
-                      </li>
-                      <li>
-                          <Link to={"/topics"}>Topics</Link>
-                      </li>
-                  </ul>
-              </nav>
+      <div className="App">
+          <Router>
+              <header className="App-header">
+                  <h1>Home</h1>
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>Welcome to our book store</p>
+                  <nav><Link to={"/"}>Home</Link> | <Link to={"/books"}>Books</Link> | <Link to={"/create"}>Create Book</Link></nav>
+              </header>
 
-              {/* A <Switch> looks through its children <Route>s and
-                renders the first one that matches the current URL. */}
+              <Books />
+
               <Switch>
-                  <Route path={"/about"}>
-                      <About />
+                  <Route path={"/books"}>
+                      <Books />
                   </Route>
-                  <Route path={"/topics"}>
-                      <Topics />
+
+                  <Route path={"/create"}>
+                      <CreateBook />
                   </Route>
                   <Route path={"/"}>
-                      <Home />
+                      <p>Home Komponente bauen...</p>
                   </Route>
               </Switch>
-          </div>
-      </Router>);
-}
-
-function Home() {
-  return (
-      <div className="App">
-          <header className="App-header">
-              <h1>Home</h1>
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                  Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-              >
-                  Learn React
-              </a>
-              <h1>Test, does it reload when i just type something</h1>
-              <p>Incredible</p>
-              <p>Tralala</p>
-        </header>
+          </Router>
       </div>
   );
 }
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Topics() {
-  let match = useRouteMatch();
-
-  return (
-      <div>
-          <h2>Topics</h2>
-
-          <ul>
-              <li>
-                <Link to={'${match.url}/components'}>Components</Link>
-              </li>
-              <li>
-                <Link to={'${match.url}/props-v-state'}>
-                  Props v. State
-                </Link>
-              </li>
-          </ul>
-
-          {/* The Topics page has its own <Switch> with more routes
-            that build on the /topics URL path. You can think of the
-            2nd <Route> here as an "index" page for all topics, or
-            the page that is shown when no topic is selected */}
-              <Switch>
-                  <Route path={`${match.path}/:topicId`}>
-                      <Topic />
-                  </Route>
-                  <Route path={match.path}>
-                      <h3>Please select a topic.</h3>
-                  </Route>
-              </Switch>
-      </div>
-  )
-}
-
-function Topic() {
-  let { topicId } = useParams();
-  return <h3>Requested topic ID: {topicId}</h3>;
-}
-
 
 
 export default App;
